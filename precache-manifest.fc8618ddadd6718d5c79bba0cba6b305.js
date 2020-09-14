ddf406d0fc63036d9f1b0d8b3938742508d2dd1c
Mohit_jha <mohitjha1511@gmail.com>
2020-09-14 11:23:35 +0530

    Updates

---
+++ precache-manifest.fc8618ddadd6718d5c79bba0cba6b305.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792ff64127f64495534da0517609ef00",
+    "url": "/A-website-with-React-js/index.html"
+  },
+  {
+    "revision": "7595dde78256feb2ae2a",
+    "url": "/A-website-with-React-js/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "be5ab8020e45a8469828",
+    "url": "/A-website-with-React-js/static/css/main.b69c3166.chunk.css"
+  },
+  {
+    "revision": "7595dde78256feb2ae2a",
+    "url": "/A-website-with-React-js/static/js/2.720d9248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/A-website-with-React-js/static/js/2.720d9248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5ab8020e45a8469828",
+    "url": "/A-website-with-React-js/static/js/main.737c81c1.chunk.js"
+  },
+  {
+    "revision": "ae16383fb80920a0bedc",
+    "url": "/A-website-with-React-js/static/js/runtime-main.eef06c04.js"
+  },
+  {
+    "revision": "45914280717e18126dd96ea33b417c48",
+    "url": "/A-website-with-React-js/static/media/about.45914280.png"
+  },
+  {
+    "revision": "5431c7bfb8a6ae3bdeafca57e306f6f2",
+    "url": "/A-website-with-React-js/static/media/android.5431c7bf.jpg"
+  },
+  {
+    "revision": "36e4531e0bbc6ee10ed70fb2785bd77c",
+    "url": "/A-website-with-React-js/static/media/app.36e4531e.jpg"
+  },
+  {
+    "revision": "90412cf3030fffab439916647b545786",
+    "url": "/A-website-with-React-js/static/media/digital.90412cf3.jpg"
+  },
+  {
+    "revision": "7a3c88c1d8f91cca797ff5d7a182f7c2",
+    "url": "/A-website-with-React-js/static/media/graphic.7a3c88c1.jpg"
+  },
+  {
+    "revision": "6a8330681a072c36543ca8a7df59cf09",
+    "url": "/A-website-with-React-js/static/media/ho.6a833068.jpg"
+  },
+  {
+    "revision": "3a7cc89111cfc5d2272c8b6e8692d549",
+    "url": "/A-website-with-React-js/static/media/home.3a7cc891.png"
+  },
+  {
+    "revision": "13fda38cecb0c3a04d76637ef181d063",
+    "url": "/A-website-with-React-js/static/media/web.13fda38c.jpg"
+  }
+]);
